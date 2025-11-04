ce190726357eb3709e48b2fb253c19e4a978c279
unknown <kionegamesyotuber337@gmail.com>
2025-11-04 08:16:45 -0300

    fix: corrige importação do App.tsx e remove index.css duplicado

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './app.css'
-import App from './App.ts
+import App from './App.tsx'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
